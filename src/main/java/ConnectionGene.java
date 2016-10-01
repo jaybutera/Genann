4d0630f3894c2379d5d56b42d9a2f3a20b9cd3ac
samendez <shanemendez223@gmail.com>
2016-10-01 14:45:18 -0500

    Created ConnectionGene

--- src/main/java/ConnectionGene.java
+++ src/main/java/ConnectionGene.java
@@ -1,2 +1,29 @@
-public class ConnectionGene {
+public final class ConnectionGene {
+  int id;
+  float weight;
+  boolean enabled;
+  NodeGene to;
+  NodeGene from;
+
+  ConnectionGene(int id, float weight, boolean enabled, NodeGene to, NodeGene from){
+    this.id = id;
+    this.weight = weight;
+    this.enabled = enabled;
+    this.to = to;
+    this.from = from;
+  }
+  ConnectionGene(ConnectionGene c){
+    this(c.id,c.weight,c.enabled,c.to,c.from);
+  }
+  ConnectionGene flip(){
+    return new ConnectionGene(c.id,c.weight,!c.enabled,c.to,c.from);
+  }
+  ConnectionGene setWeight(float w){
+    return new ConnectionGene(c.id,w,c.enabled,c.to,c.from);
+  }
+  boolean isEnabled(){
+    return enabled;
+  }
+
+
 }
