8b689e4d93263065a8a5c5d96ef34b99844259d1
d7laungani <d7laungani@hotmail.com>
2016-10-01 17:30:27 -0500

    changed weight to double and added enable and disable

--- src/main/java/ConnectionGene.java
+++ src/main/java/ConnectionGene.java
@@ -1,12 +1,13 @@
 public final class ConnectionGene {
 
-	NodeGene from;
+
+    NodeGene from;
   NodeGene to;
-  float weight;
+  double weight;
   boolean enabled;
 	int id;
 
-  ConnectionGene(NodeGene from, NodeGene to, float weight, boolean enabled, int id){
+  ConnectionGene(NodeGene from, NodeGene to, double weight, boolean enabled, int id){
 		this.from = from;
     this.to = to;
 		this.weight = weight;
@@ -20,7 +21,14 @@ public final class ConnectionGene {
   ConnectionGene flip(){
     return new ConnectionGene(this.from,this.to,this.weight,!this.enabled,this.id);
   }
-  ConnectionGene setWeight(float w){
+
+    ConnectionGene endable(){
+        return new ConnectionGene(this.from,this.to,this.weight,true,this.id);
+    }
+    ConnectionGene disable(){
+        return new ConnectionGene(this.from,this.to,this.weight,false,this.id);
+    }
+  ConnectionGene setWeight(double w){
     return new ConnectionGene(this.from,this.to,w,this.enabled,this.id);
   }
   boolean isEnabled(){
