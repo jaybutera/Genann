a0b055544a4f42d730f698961cc72349ef95cf30
Jay Butera <buterajay@gmail.com>
2016-10-01 19:03:38 -0500

    Add test

---
+++ src/main/java/TestGA.java
@@ -0,0 +1,29 @@
+public class TestGA {
+    public static void main(String args[]) {
+        XOR f = new XOR();
+        Population p = new Population(100, .1, .1, .1, .3, 1.3, 2, 1, f);
+
+        p = p.nextGen();
+        Genome init_fit = p.getMostFit();
+
+        // Run through 100 generations
+        for (int i = 0; i < 5; i++) {
+            System.out.println("Running generation " + (i+1) + "...");
+            //System.out.println("Number of species: " + p.getNumSpecies());
+            p = p.nextGen();
+            System.out.println("Most fit genome:\n\n" + p.getMostFit());
+            System.out.println("Top fitness: " + f.simulate( new Network(p.getMostFit()) ));
+        }
+
+        Genome g = p.getMostFit();
+        System.out.println(g);
+        Network n = new Network(g);
+        System.out.println(n);
+        System.out.print("Output: ");
+        Double[] outs = f.getOuts();
+        for (int i = 0; i < outs.length; i++)
+            System.out.print(outs[i] + ", ");
+        System.out.println("");
+        System.out.println("Convergence success: " + (1 - g.fitness / init_fit.fitness) + " %");
+    }
+}
---
+++ src/main/java/XOR.java
@@ -0,0 +1,45 @@
+public class XOR extends Fitness {
+    public Double simulate (Network n) {
+        outs = new Double[outData.length][1];
+
+        try {
+            for (int i = 0; i < inData.length; i++) {
+                outs[i] = n.step(inData[i]);
+                //System.out.print(outs[i][0] + " : ");
+            }
+        }
+        catch (Exception e) {
+            System.out.println("Invalid network\n" + n);
+        }
+
+        Double fitness = 0.0;
+        for (int i = 0; i < outs.length; i++)
+            fitness += Math.abs(outs[i][0] - outData[i]);
+
+        //System.out.println("Fitness - " + fitness);
+        return Math.pow(outs.length - fitness,2);
+    }
+
+    public Double[] getOuts () {
+        Double[] x = new Double[outs.length];
+
+        // Transpose outs
+        for (int i = 0; i < outs.length; i++)
+            x[i] = outs[i][0];
+
+        return x;
+    }
+
+    private Double[][] outs; // Calculated output
+    private Double[][] inData = {{0.,0.},
+                                 {1.,0.},
+                                 {0.,1.},
+                                 {1.,1.}};
+    /*
+    private Double[][] inData = {{1.,1.,1.},
+                                 {1.,1.,0.},
+                                 {1.,0.,1.},
+                                 {1.,0.,0.}};
+                                 */
+    private Double[] outData = {0.,1.,1.,0.};
+}
