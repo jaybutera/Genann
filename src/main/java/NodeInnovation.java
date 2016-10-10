5364692e3e6440c0c00cf619b8f98a31dc62175f
Devesh <d7laungani@hotmail.com>
2016-10-10 11:33:54 -0500

    Add NodeInnovation packager

---
+++ src/main/java/NodeInnovation.java
@@ -0,0 +1,22 @@
+/**
+ * Created by Devesh on 10/9/2016.
+ */
+
+
+// Only used for tuple equivalent for returning information from a function (addNode) in innovationDB
+public class NodeInnovation {
+
+    NodeGene n;
+    ConnectionGene c1;
+    ConnectionGene c2;
+
+
+    NodeInnovation (NodeGene n,ConnectionGene c1, ConnectionGene c2) {
+
+        this.n = n;
+        this.c1 = c1;
+        this.c2 = c2;
+
+    }
+
+}
