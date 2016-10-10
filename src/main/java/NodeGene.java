9c241da977eebef5f94745b85b7466834d337615
Devesh <d7laungani@hotmail.com>
2016-10-09 23:07:18 -0500

    Add overriden equals func for comparison

--- src/main/java/NodeGene.java
+++ src/main/java/NodeGene.java
@@ -1,14 +1,18 @@
 public class NodeGene {
-
-
     public int id;
 
     NodeGene(int geneCount){
-
-        this.id = ++geneCount;
+        this.id = geneCount;
     }
 
-    public boolean equals (ConnectionGene c){
+    @Override
+    public boolean equals (Object o){
+        NodeGene c = (NodeGene) o ;
+        return this.id == c.id;
+    }
+
+
+    public boolean equals (NodeGene c){
         return this.id == c.id;
     }
 }
