c6fd80a65408b5da7e5fee57047c0600ea8ddefb
samendez <shanemendez223@gmail.com>
2016-10-16 03:32:53 -0500

    Removed syncronisity from Genome, getting started on doing same to chromosome

--- src/main/java/Genetics/Genome.java
+++ src/main/java/Genetics/Genome.java
@@ -2,37 +2,84 @@ package Genetics;
 
 import java.util.ArrayList;
 import java.util.Random;
-import java.util.stream.Collectors;
-import java.util.Comparator;
+import java.util.Iterator;
 
-public final class Genome {
+public class Genome {
 
     // These should be private
-    public Random r = new Random();
-    public Chromosome<ConnectionGene> chromosome;
+    public Random random = new Random();
+    public Chromosome<Gene> chromosome;
     private int nodes = 1;
     private InnovationDB db;
-    
-    public Genome(int nodes, InnovationDB db){
+    private double mrate = .2;
+    private double srate = .3;
+
+    public Genome(int nodes, InnovationDB db) {
         this(new Chromosome(), nodes, db);
     }
-    public Genome(Chromosome<ConnectionGene> chromosome, int nodes, InnovationDB db) {
-       this.chromosome = chromosome;
-       this.nodes = nodes;
-       this.db = db;
+
+    public Genome(Chromosome chromosome, int nodes, InnovationDB db) {
+        this.chromosome = chromosome;
+        this.nodes = nodes;
+        this.db = db;
     }
-    public Genome addNode(){
-        return new Genome(chromosome, nodes+1, db);
+
+    public Genome addNode() {
+        nodes += 1;
+        return this;
     }
-    public Genome addConnection(int from, int to, double weight){
-        ConnectionGene conn = new ConnectionGene(from, to, weight, true);
-        int id = db.get(conn);
-        conn.setId(id);
-        Chromosome<ConnectionGene> conns = chromosome.add(conn);
-        return new Genome(conns, nodes, db);
+
+    public Genome addAll(ArrayList genes) {
+        chromosome.addAll(genes);
+        return this;
     }
-    public String toString(){
-        return nodes+", "+chromosome.toString();
+
+    public Genome add(Gene g) {
+        int id = db.get(g);
+        g.setId(id);
+        chromosome.add(g);
+        System.out.println(chromosome);
+
+        return this;
     }
-   
+
+    public Genome remove(Gene g) {
+        chromosome.remove(g);
+        return this;
+    }
+
+    @Override
+    public String toString() {
+        return nodes + ", " + chromosome.toString();
+    }
+
+    @Override
+    public Genome clone() {
+        return new Genome(this.chromosome, nodes, db);
+    }
+
+    /**
+     * Only parts that cannot be separated from ConnectionGene Splitting is a
+     * feature of only connection genes maybe in the future we can make this
+     * less specific
+     *
+     * @return
+     */
+    public Genome split(ConnectionGene c) {
+        this.addAll(c.split(nodes));
+        this.remove(c);
+
+        return this;
+    }
+
+    public Genome mutate() {
+        this.chromosome.mutate(mrate);
+        if (this.srate < this.random.nextDouble()) {
+
+        }
+
+        return this;
+
+    }
+
 }
